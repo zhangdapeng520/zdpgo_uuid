db7a550cf8a920023b510ddcd4f92a070359ec87
zhangdapeng520 <82651084+zhangdapeng520@users.noreply.github.com>
2022-07-24 23:29:43 +0800

    新增：获取UUID字符串

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/zhangdapeng520/zdpgo_uuid
 
-go 1.17
+go 1.18
--- uuid_test.go
+++ uuid_test.go
@@ -5,22 +5,6 @@ import (
 	"testing"
 )
 
-func getUuid() *Uuid {
-	return NewWithConfig(&Config{})
-}
-
 func TestUUID(t *testing.T) {
-	fmt.Println(UUID())
-}
-
-func TestUuid_String(t *testing.T) {
-	u := getUuid()
-	s1 := u.String()
-
-	for i := 0; i < 10000; i++ {
-		s2 := u.String()
-		if s1 == s2 {
-			panic("UUID重复了")
-		}
-	}
+	fmt.Println(String())
 }
